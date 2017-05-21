7009b6cedd9286c0f63a0e570bf23a5eb0461838
Matthew Upson <matthew.a.upson@gmail.com>
2017-05-21 13:06:18 +0000

    Populate docker-compose with env variables

---
+++ mqtt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd /home/pi/home-automation
+
+# See http://lukeswart.net/2016/03/lets-deploy-part-1/
+# env variables in docker-compose seem a bit patchy.
+# This bash workaround works well.
+
+sudo docker rm -f `docker ps -aq -f 'name=homeautomation_*'`
+set -a
+source .env
+cat docker-compose.yml | envsubst | sudo docker-compose -f - up
